8739c4de0b7160c3b34bb5fc53be1466422cfb4e
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2018-08-29 14:12:31 -0500

    Solution code

--- allAnagrams.js
+++ allAnagrams.js
@@ -12,38 +12,10 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
   //create a function called all anagrams
-  var allAnagrams = function(string) {
-    //define placeholders
-     var newString = '';
-     var newArr = [];
-     //iterate through the string
-     for (var i = 0; i < string.length; i += 1) {
-       //add the string index into a value
-       var first = string[i];
-       //iterate a seccond time
-       for (var j = 0; j < string.length; j += 1) {
-         //add the string index into the second value
-         var second = string[j];
-         //iterate the string
-         for (var k = 0; k < string.length; k += 1) {
-           //add the value into the thrird value
-           var third = string[k];
-           //if the values aren't euqal to each other then
-          if (first !== second && first !== third && second !== third && second !== first) {
-            // push them into the placeholder
-           newArr.push(first + second +third);
-          }
+ var allAnagrams = function(string) {
 
-         }
-       }
-     }
-     //return the value of the new array
-     return newArr;
-   };
-   /*
-    hack reactor's : 
-   allAnagrams = function(string) {
 var anagrams = {};
+
 var generator = function(text, options){
   if (text.length === string.length){
     anagrams[text] = true
@@ -56,4 +28,3 @@ var generator = function(text, options){
 generator(‘’, string);
 return Object.keys(anagrams);
 }
-   */
